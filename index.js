d95c5a26a14045e6f3060b5530a615d0711cecd3
hizkielT <hezekiel.feyissa@gmail.com>
2022-12-18 13:05:55 +0300

    come on

--- index.js
+++ index.js
@@ -40,10 +40,10 @@ io.on("connection", (socket) => {
 })
 
 if(process.env.NODE_ENV ===  'production') {
-  app.use(express.static('frontend/build'))
+  app.use(express.static(path.join(__dirname,"frontend/build")));
 
   app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   } )
 }
 
