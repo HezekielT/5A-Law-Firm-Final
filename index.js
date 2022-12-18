fb789e11a5b2428cde9d165a64b5cb3e7e0d79e0
hizkielT <hezekiel.feyissa@gmail.com>
2022-12-18 13:21:42 +0300

    should've replaced frontend by client

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ io.on("connection", (socket) => {
 })
 const __dirname1 = path.resolve();
 if(process.env.NODE_ENV ===  'production') {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("client/build"));
 
   app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   } )
 }
 
