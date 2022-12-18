d53c56bd514c0ccb1b9bc8c1f8da2525ffbd187c
hizkielT <hezekiel.feyissa@gmail.com>
2022-12-18 13:50:48 +0300

    cors origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ['http://localhost:3000'],
+    origin: true,
   }
 })
 // io.set('origins','*:*');
