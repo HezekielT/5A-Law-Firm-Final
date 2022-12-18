543e1956fa9ea3d437d64ed0f357599f19aecb32
hizkielT <hezekiel.feyissa@gmail.com>
2022-12-18 13:16:05 +0300

    it may have worked

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ io.on("connection", (socket) => {
     socket.broadcast.emit('get-delete-schedule', id);
   })
 })
-// const __dirname1 = path
+const __dirname1 = path.resolve();
 if(process.env.NODE_ENV ===  'production') {
-  app.use(express.static("frontend/build"));
+  app.use(express.static(path.join(__dirname1,"frontend/build")));
 
   app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+      res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
   } )
 }
 
